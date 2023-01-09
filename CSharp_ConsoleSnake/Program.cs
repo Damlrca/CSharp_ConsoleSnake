24156f1f886c0c0960776c44a19a8ff1236357ae
Damlrca <sir.damirca@yandex.ru>
2023-01-09 03:27:37 +0300

    its running fast as never

--- CSharp_ConsoleSnake/Program.cs
+++ CSharp_ConsoleSnake/Program.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace CSharp_ConsoleSnake
 {
     internal class Program
     {
         static int score;
+        static private int FrameDelay = 300;
         static void Main(string[] args)
         {
             Console.CursorVisible = false;
@@ -18,23 +19,23 @@ namespace CSharp_ConsoleSnake
 
             Console.SetWindowSize(m.GetLength(1) * 2 + 4, m.GetLength(0) + 2);
 
-            for (int i = 0; i < m.GetLength(0) + 2; i++)
+            for (int i = 0; i < m.GetLength(1) + 2; i++)
                 DrawPoint(i, 0, PointType.Border);
 
-            for (int j = 1; j < m.GetLength(1) + 1; j++)
-                DrawPoint(0, 2 * j, PointType.Border);
+            for (int i = 0; i < m.GetLength(1) + 2; i++)
+                DrawPoint(i, m.GetLength(0) + 1, PointType.Border);
 
-            for (int i = 0; i < m.GetLength(0) + 2; i++)
-                DrawPoint(i, 2 * (m.GetLength(1) + 1), PointType.Border);
+            for (int i = 1; i < m.GetLength(0) + 1; i++)
+                DrawPoint(0, i, PointType.Border);
 
-            for (int j = 1; j < m.GetLength(1) + 1; j++)
-                DrawPoint(m.GetLength(0) + 1, 2 * j, PointType.Border);
+            for (int i = 1; i < m.GetLength(0) + 1; i++)
+                DrawPoint(m.GetLength(1) + 1, i, PointType.Border);
 
             Queue<Point> snake = new Queue<Point>();
 
             Point head = new Point(0, 0);
 
-            for (int i = 1; i <= 5; i++)
+            for (int i = 1; i <= 10; i++)
             {
                 m[1, i] = PointType.Snake;
                 Point temp = new Point(1, i);
@@ -46,40 +47,49 @@ namespace CSharp_ConsoleSnake
 
             ConsoleKeyInfo pressedKey = new ConsoleKeyInfo(' ', ConsoleKey.RightArrow, false, false, false);
 
-            Task.Run(() =>
-            {
-                while (true)
-                {
-                    ConsoleKeyInfo temp = Console.ReadKey(true);
-                    switch (temp.Key)
-                    {
-                        case ConsoleKey.LeftArrow:
-                            if (pressedKey.Key != ConsoleKey.RightArrow)
-                                pressedKey = temp;
-                            break;
-                        case ConsoleKey.RightArrow:
-                            if (pressedKey.Key != ConsoleKey.LeftArrow)
-                                pressedKey = temp;
-                            break;
-                        case ConsoleKey.UpArrow:
-                            if (pressedKey.Key != ConsoleKey.DownArrow)
-                                pressedKey = temp;
-                            break;
-                        case ConsoleKey.DownArrow:
-                            if (pressedKey.Key != ConsoleKey.UpArrow)
-                                pressedKey = temp;
-                            break;
-                        default:
-                            break;
-                    }   
-                }
-                    
-            });
+            Stopwatch sw = new Stopwatch();
+
+            Redraw(m);
 
             while (true)
             {
+                sw.Restart();
+
+                ConsoleKeyInfo oldKey = pressedKey;
+                
+                //while (sw.ElapsedMilliseconds <= FrameDelay && oldKey == pressedKey)
+                //{
+                    if (Console.KeyAvailable)
+                    {
+                        ConsoleKeyInfo temp = Console.ReadKey(true);
+                        switch (temp.Key)
+                        {
+                            case ConsoleKey.LeftArrow:
+                                if (pressedKey.Key != ConsoleKey.RightArrow)
+                                    pressedKey = temp;
+                                break;
+                            case ConsoleKey.RightArrow:
+                                if (pressedKey.Key != ConsoleKey.LeftArrow)
+                                    pressedKey = temp;
+                                break;
+                            case ConsoleKey.UpArrow:
+                                if (pressedKey.Key != ConsoleKey.DownArrow)
+                                    pressedKey = temp;
+                                break;
+                            case ConsoleKey.DownArrow:
+                                if (pressedKey.Key != ConsoleKey.UpArrow)
+                                    pressedKey = temp;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                //}
+
                 m[head.X, head.Y] = PointType.Snake;
-                switch (pressedKey.Key)
+                DrawPoint(head.Y + 1, head.X + 1, m[head.X, head.Y]);
+                ConsoleKey nowPressed = pressedKey.Key; 
+                switch (nowPressed)
                 {
                     case ConsoleKey.LeftArrow:
                         head.Y = (head.Y + m.GetLength(1) - 1) % m.GetLength(1);
@@ -102,9 +112,13 @@ namespace CSharp_ConsoleSnake
                 Point back = snake.Dequeue();
                 m[back.X, back.Y] = PointType.Empty;
 
-                Redraw(m);
+                //Redraw(m);
+                DrawPoint(head.Y + 1, head.X + 1, m[head.X, head.Y]);
+                DrawPoint(back.Y + 1, back.X + 1, m[back.X, back.Y]);
 
-                Thread.Sleep(300);
+                int t = 200 - (int)sw.ElapsedMilliseconds;
+                if (t > 0)
+                    Thread.Sleep(t);
             }
         }
 
@@ -117,12 +131,12 @@ namespace CSharp_ConsoleSnake
         {
             for (int i = 0; i < m.GetLength(0); i++)
                 for (int j = 0; j < m.GetLength(1); j++)
-                    DrawPoint(i + 1, 2 * j + 2, m[i, j]);
+                    DrawPoint(j + 1, i + 1, m[i, j]);
         }
 
-        static void DrawPoint(int x, int y, PointType p)
+        static void DrawPoint(int left, int top, PointType p)
         {
-            Console.SetCursorPosition(y, x);
+            Console.SetCursorPosition(2 * left, top);
             switch (p)
             {
                 case PointType.Empty:
